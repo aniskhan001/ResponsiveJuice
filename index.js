bb498fdeece1456365f0eec67bb7e6eeade18088
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-05-23 20:20:46 +0600

    sample markup test added

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-var express = require('express')
-var app = express()
+// Server config
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	response.send('Hello World!');
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+	console.log("Node app is running at localhost:" + app.get('port'));
 })
+
+// Juice test
+var fs 	 = require("fs");
+var html = fs.readFileSync("juice/index.html").toString('utf8');
+var css  = fs.readFileSync("juice/style.css").toString('utf8');
+
+// console.log(html);
+var juice	= require('juice');
+var result	= juice.inlineContent(html, css);
+// console.log(result);
+
+fs.writeFileSync("juice/rendered.html", result, 'utf8');
+// console.log(result);
+
