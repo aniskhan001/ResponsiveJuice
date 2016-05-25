6ef101113e62111c4f8180a5a3ab0bd1da8ba04b
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-05-25 19:25:59 +0600

    route testing

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('juice/', function(request, response) {
+  response.send('Juice World!')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
