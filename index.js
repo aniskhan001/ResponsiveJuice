795726c4de015068da90aec5fb8448690f0c573a
Anisuzzaman Khan <aniskhan001@outlook.com>
2016-05-25 20:14:37 +0600

    position changed

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var express = require('express')
 var http	= require('http')
 var fs 		= require('fs')
 var juice	= require('juice')
+var bodyParser = require('body-parser')
 
 var app	= express()
-var bodyParser = require('body-parser')
+
 
 // app.use(bodyParser.json()) // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
